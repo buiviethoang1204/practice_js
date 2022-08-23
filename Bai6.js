7c6c687e1636a4ec74937d7feafc1da42ff6f02c
buiviethoang1204 <buiviethoang120498@gmail.com>
2022-08-23 13:11:43 +0700

    fix bai 6

--- Bai6.js
+++ Bai6.js
@@ -1,10 +1,10 @@
 function filterRangeInPlace(arr, a, b) {
+    const min = Math.min(a, b);
+    const max = Math.max(a, b);
     return arr.reduce((acc, curr) => {
-        if (a < b) {
-            return (curr >= a && curr <= b) ? acc.concat(curr) : acc;
-        }
-        return (curr >= b && curr <= a) ? acc.concat(curr) : acc;
+        return (curr >= min && curr <= max) ? acc.concat(curr) : acc;
     }, [])
 }
 arr = [3, 4, 6, 2, 1, 5];
-console.log(filterRangeInPlace(arr, 3, 1));
+console.log(filterRangeInPlace(arr, 1, 4));
+console.log(arr);
