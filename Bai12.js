37d21aaf7c9ca39b803ff6f0bfa3cc08c16b7917
buiviethoang1204 <buiviethoang120498@gmail.com>
2022-08-29 13:33:53 +0700

    fix bai 12, 14

--- Bai12.js
+++ Bai12.js
@@ -1,17 +1,9 @@
 const arr1 = [1, 2, 3, 4, 5];
 const arr2 = [2, 3, 6];
 function differentArr(arr1, arr2) {
-    let result = [];
-    arr1.reduce((acc, curr) => {
-        if (arr2.indexOf(curr) === -1) {
-            result.push(curr);
-        }
+    return arr1.reduce((acc, curr) => {
+        (arr2.indexOf(curr) === -1) ? acc.push(curr) : acc;
+        return acc;
     }, [])
-    arr2.reduce((acc, curr) => {
-        if (arr1.indexOf(curr) === -1) {
-            result.push(curr);
-        }
-    }, [])
-    return result;
 }
 console.log(differentArr(arr1, arr2));
--- Bai14.js
+++ Bai14.js
@@ -1,7 +1,6 @@
 const arr = [0, 1, 1, 2, 2, 2];
 const result = arr.reduce((acc, curr) => {
-    if (acc[curr] = (acc[curr] || 0) + 1) {
-        return acc;
-    }
+    acc[curr] = (acc[curr] || 0) + 1
+    return acc;
 }, {})
 console.log(result);
