0bc7f68f2734f446113bb1a330e80cae839c5a08
buiviethoang1204 <84034624+buiviethoang1204@users.noreply.github.com>
2022-09-19 09:46:35 +0700

    Delete Bai12.js

--- Bai12.js
+++
@@ -1,9 +0,0 @@
-const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [2, 3, 6];
-function differentArr(arr1, arr2) {
-    return arr1.reduce((acc, curr) => {
-        (arr2.indexOf(curr) === -1) ? acc.push(curr) : acc;
-        return acc;
-    }, [])
-}
-console.log(differentArr(arr1, arr2));
