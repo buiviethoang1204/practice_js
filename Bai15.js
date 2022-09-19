fcbcfcd07e8ddeb4658d4e8849c37799429b58c7
buiviethoang1204 <84034624+buiviethoang1204@users.noreply.github.com>
2022-09-19 09:46:57 +0700

    Delete Bai15.js

--- Bai15.js
+++
@@ -1,9 +0,0 @@
-const arr = [12, 45, 21, 65, 38, 76, 108, 43];
-const maxArr = arr.reduce((acc, curr) => {
-    return curr > acc ? curr : acc;
-})
-const minArr = arr.reduce((acc, curr) => {
-    return curr < acc ? curr : acc;
-})
-console.log(maxArr);
-console.log(minArr);
